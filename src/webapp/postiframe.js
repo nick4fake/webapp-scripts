465ff833b7b1f0436a1c6cedb475c550fcfbb2cb
Bogdan Yurov <bogdan@yurov.me>
2013-09-21 16:57:17 +0300

    post fix

--- src/webapp/postiframe.js
+++ src/webapp/postiframe.js
@@ -1,27 +1,36 @@
 app.fn.postiframe = function (href, optionsin, data) {
-	var pref = app.queryPrefix ? app.queryPrefix + '/' : '';
-	var options = {
-		fitToView: false,
-		autoSize:  true,
-		href:      pref + href
-	};
-	$.extend(options, optionsin);
-	$.extend(options, {
-		type:      'iframe',
-		afterLoad: (function () {
-			var frame = $(this.content[0]);
-			var form = $('<form action="' + options.href + '" method="POST"></form>');
-			for (var name in data) {
-				form.append(
-					$('<input type="hidden" name value />').
-						attr('name', name).
-						attr('value', (typeof data[name] == 'object') ? $.toJSON(data[name]) : data[name])
-				);
-			}
+    var pref = app.queryPrefix ? app.queryPrefix + '/' : '';
+    var options = {
+        fitToView: false,
+        autoSize:  true,
+        href:      pref + href
+    };
+    var toString = function (el) {
+        if (!el && el !== 0) {
+            return '';
+        }
+        if (typeof el == 'object') {
+            return $.toJSON(el);
+        }
+        return el;
+    };
+    $.extend(options, optionsin);
+    $.extend(options, {
+        type:      'iframe',
+        afterLoad: (function () {
+            var frame = $(this.content[0]);
+            var form = $('<form action="' + options.href + '" method="POST"></form>');
+            for (var name in data) {
+                form.append(
+                    $('<input type="hidden"/>').
+                        attr('name', name).
+                        attr('value', toString(data[name]))
+                );
+            }
 
-			frame.contents().find('body').html('').append(form);
-			form.submit();
-		})
-	});
-	$.fancybox(options);
+            frame.contents().find('body').html('').append(form);
+            form.submit();
+        })
+    });
+    $.fancybox(options);
 };
